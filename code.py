7d1ddd36abe138ccf28cff0aa2a2e50fd553f9e1
guitsuo <guilhermeoshiro@outlook.com>
2024-03-16 09:25:03 -0300

    adding langchain react + agent

--- code.py
+++ code.py
@@ -1,5 +1,25 @@
+
+
 import numpy as np 
 import pandas as pd 
+import torch 
 
 
-print('hi')
+from huggingface_hub import notebook_login
+
+from langchain_community.llms import HuggingFaceEndpoint
+from langchain_community.chat_models.huggingface import ChatHuggingFace
+
+# Enter your token ID (from huggingface)
+token = ''
+
+notebook_login()
+
+
+# Check if GPU is set  
+torch.cuda.is_available()
+
+
+llm = HuggingFaceEndpoint(repo_id="HuggingFaceH4/zephyr-7b-beta", huggingfacehub_api_token = token)
+
+chat_model = ChatHuggingFace(llm=llm)
